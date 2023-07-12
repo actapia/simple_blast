ef1a2d968908ae6e7368108d1df63b7a56724605
actapia <andrew.tapia@uky.edu>
2023-07-11 22:13:47 -0400

    Allow BlastDBCache to find existing databases.

--- src/simple_blast/blastdb_cache.py
+++ src/simple_blast/blastdb_cache.py
@@ -2,12 +2,26 @@ import subprocess
 import tempfile
 from pathlib import Path
 
+def read_nal_title(nal):
+    with open(nal, "r") as nal_file:
+        for l in nal_file:
+            if not l.startswith("#"):
+                split = l.rstrip().split(" ")
+                if split[0] == "TITLE":
+                    return " ".join(split[1:])
+            
+
 class BlastDBCache:
-    def __init__(self, location):
+    def __init__(self, location, find_existing=True):
         self.location = location
         self._cache = {}
+        if find_existing:
+            for path in Path(location).glob("*/*.nal"):
+                self._cache[read_nal_title(path)] = path.parent                
 
     def makedb(self, seq_file_path):
+        if seq_file_pth in self._cache:
+            return
         seq_file_path = Path(seq_file_path)
         seq_name = seq_file_path.stem
         tempdir = Path(
