0bb78997ceeebf710ec47e6d6e1d2bf1e0ad60c6
actapia <andrew.tapia@uky.edu>
2023-08-24 13:26:36 -0400

    Support -parse_seqids

--- src/simple_blast/blastdb_cache.py
+++ src/simple_blast/blastdb_cache.py
@@ -12,12 +12,32 @@ def read_nal_title(nal):
             
 
 class BlastDBCache:
-    def __init__(self, location, find_existing=True):
+    def __init__(self, location, find_existing=True, parse_seqids=False):
         self.location = location
         self._cache = {}
         if find_existing:
             for path in Path(location).glob("*/*.nal"):
                 self._cache[Path(read_nal_title(path))] = path.parent / "db"
+        self._parse_seqids = parse_seqids
+
+    @property
+    def parse_seqids(self):
+        return self._parse_seqids
+
+    def _build_makeblastdb_command(self, seq_file_path, db_name):
+        command = [
+                "makeblastdb",
+                "-in",
+                str(seq_file_path),
+                "-out",
+                db_name,
+                "-dbtype",
+                "nucl",
+                "-hash_index" # Do I need this?
+        ]
+        if self._parse_seqids:
+            command.append("-parse_seqids")
+        return command
 
     def makedb(self, seq_file_path):
         seq_file_path = Path(seq_file_path)
@@ -32,16 +52,7 @@ class BlastDBCache:
         )
         db_name = str(tempdir / "db")
         proc = subprocess.Popen(
-            [
-                "makeblastdb",
-                "-in",
-                str(seq_file_path),
-                "-out",
-                db_name,
-                "-dbtype",
-                "nucl",
-                "-hash_index" # Do I need this?
-            ],
+            self._build_makeblastdb_command(seq_file_path, db_name),
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL
         )
